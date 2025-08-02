a9799b036458803ac970f6c083c9307445844139
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-08-02 18:17:29 +0100

    fix local runner

---
+++ start_local.bat
@@ -0,0 +1,3 @@
+cd "C:\Users\drala\Documents\Projects\GitHub\HamStatsLib"
+start http://localhost:8000
+python -m http.server
--- test/launchLocal.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\Users\drala\Documents\Projects\GitHub\HamStatsLib\test"
-start http://localhost:8000
-python -m http.server
